43cddd504825c6aed5befc1cf66646ab121e7e31
Adam Clements <awc919@gmail.com>
2023-02-05 01:09:45 -0500

    Ready to merge
    Ready to merge

--- README.md
+++ README.md
@@ -9,3 +9,12 @@ Web scraper APIs - Python
 Frontend - Django (+ React?)
 
 Hosting: AWS
+
+
+Data Manip: Grade Dist -> clean.csv (cleanup.m)
+
+Web Scraper APIs: RMP + Clemson Course Catalog -> Pandas df + vars via pullData.py via APIs.py
+
+Frontend: God if either of us know what magic makes a frontend work
+
+Hosting: Not there yet, will do AWS maybe with a .tech domain
--- pullData.py
+++ pullData.py
@@ -6,7 +6,7 @@ import APIs as api
 df = pd.read_csv("clean.csv")
 
 courseName = "CPSC"
-courseNumber = "3120"
+courseNumber = "2150"
 
 df = df[df.apply(lambda row: row.astype(str).str.contains(courseName).any(), axis=1)]
 df = df[df.apply(lambda row: row.astype(str).str.contains(courseNumber).any(), axis=1)]
@@ -15,6 +15,9 @@ df = df[df.apply(lambda row: row.astype(str).str.contains(courseNumber).any(), a
 print(api.getCourseInfo(courseName, courseNumber))
 col_list = df[df.columns[12]].values.tolist()
 
+gradeIndex = ['A', 'B', 'C', 'D', 'F', 'W', 'I', 'P', 'N(P)']
+grades_df = df[gradeIndex]
+
 for i in range (0,len(col_list)):
     lastname = col_list[i].split(',')[0]
     firstname = col_list[i].split(',')[1]
@@ -22,13 +25,7 @@ for i in range (0,len(col_list)):
 
     scores = api.getRMPInfo(firstname, lastname)
 
-    print(scores)
-
-    
-
 print(df)
-
-df.to_json("Dataframe.json")
-
+print(grades_df)
 
 
