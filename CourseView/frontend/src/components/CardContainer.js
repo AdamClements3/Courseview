17b5daa4cd9b38b3624e0e13d30387aba5e9d0e8
qwerty682 <squan710@gmail.com>
2023-02-05 08:38:15 -0500

    make it look pretty

--- CourseView/frontend/src/components/CardContainer.js
+++ CourseView/frontend/src/components/CardContainer.js
@@ -2,10 +2,22 @@ import * as React from "react"
 import Card from './Card.js'
 
 function CardContainer(props) {
+    
     console.log(props.data);
-    var prof1 = props.data[0];
-    var prof2 = props.data[1];
-    var prof3 = props.data[2];
+    if (props.data.length < 2)
+    {
+        var backEnd = [props.data[0], [[''], [''], [''], [''], [''], [''], [''], [''], [''], [''], [''], [''], [''], ['']], [[''], [''], [''], [''], [''], [''], [''], [''], [''], [''], [''], [''], [''], ['']]];
+    }
+    else if (props.data.length < 3)
+    {
+        var backEnd = [props.data[0], props.data[1],[[''], [''], [''], [''], [''], [''], [''], [''], [''], [''], [''], [''], [''], ['']]];
+    }
+    else
+        var backEnd = props.data
+
+    var prof1 = backEnd[0];
+    var prof2 = backEnd[1];
+    var prof3 = backEnd[2];
     return (
         <div>
             <Card name = {prof1[0]} body = {prof1[2,3,4,5,6,7,8,9,10]}></Card>
